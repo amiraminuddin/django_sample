6e30ef65202dfd3581ee02b56462a80ff8b7d2ed
amiraminuddin <amiraminuddin@gmail.com>
2020-10-24 17:14:49 +0800

    Initial Commit

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+#from django.http import HttpResponse 
+
+#this one is dummy data
+posts = [
+    {
+        'author':'don',
+        'title':'Blog post 1',
+        'content':'First post content',
+        'date_posted':'October 23, 2020'
+    },
+    {
+        'author':'pekeh',
+        'title':'Blog post 2',
+        'content':'second post content',
+        'date_posted':'October 24, 2020'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+
+    #dictionary for data posts
+    context = {
+        'title': 'welcome',
+        'posts': posts
+    }
+
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
+
