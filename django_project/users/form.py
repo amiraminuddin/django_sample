4a84467fd3554c8dee536375df419306aba6e095
amiraminuddin <amiraminuddin@gmail.com>
2020-10-30 10:51:31 +0800

    commit

--- django_project/users/form.py
+++ django_project/users/form.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
+#class that inherit from UserCreationForm
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
+
     class Meta:
         model = User
+        #field that will be shown
         fields = ['username', 'email', 'password1', 'password2']
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -5,12 +5,17 @@ from .form import UserRegisterForm
 
 # Create your views here.
 def register(request):
+    #checking for post request
     if request.method == 'POST':
+        #create a new form
         form = UserRegisterForm(request.POST)
+        #check the form valid
         if form.is_valid():
             form.save()
+            #find all validated form data
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
+            #return the message to blog
             return redirect('blog-home')
     else:
         form = UserRegisterForm()
