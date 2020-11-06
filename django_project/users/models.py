9a98374267379360b2c8a01e996e280764a0a99e
amiraminuddin <amiraminuddin@gmail.com>
2020-11-06 11:46:01 +0800

    commit

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+# create a model for one to one relationship
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
